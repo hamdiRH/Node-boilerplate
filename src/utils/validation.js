5afb24a5647cdbeb1c474d4417fe1b59b8ef664d
hamdiRH <49039080+hamdiRH@users.noreply.github.com>
2020-07-27 18:03:17 +0100

    new_features

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -88,6 +88,7 @@ export const schema = {
   isNotVerified:{
     custom: {
       options: (value) => {
+        console.log(value)
         return User.find({ email: value }).then((user) => {
           if (user.state.emailVerified) {
             return Promise.reject(new Error());
